20cd1783d61d8ab99d78503343db278bed364d9b
dangkjoabi80Âgmail.com <dangkhoabi80@gmail.com>
2026-05-21 14:29:45 +0700

    Resolve merge conflicts

---
+++ FE/src/assets/logo/Logo.jsx
@@ -0,0 +1,9 @@
+import "./Logo.css";
+function Logo() {
+    return (
+        <div className="logo">
+            <div className="logo_icon">✦</div>
+        </div>
+    );
+}
+export default Logo;
