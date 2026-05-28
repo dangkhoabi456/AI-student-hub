0fee4f98a46df454e62763fc856be0ace94486bd
Duc Trong <buivoductrong1234@gmail.com>
2026-05-28 13:52:34 +0700

    update google_client_id

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // Client ID lấy từ ảnh chụp của bạn
-const GOOGLE_CLIENT_ID = "8162820576094clrdj4f4mp1jh72m40ffaf04fne6vhe.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "816282057609-4clrdj4f4mp1jh72m40ffaf04fne6vhe.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
